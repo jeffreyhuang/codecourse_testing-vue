be4cb7c93799509f3ec0ecfb770e8408285aeab8
Jeffrey Huang <jeffreyhuang0331@gmail.com>
2019-06-25 11:02:27 +0800

    set up the project

---
+++ src/components/test.spec.js
@@ -0,0 +1,10 @@
+import { mount } from '@vue/test-utils'
+import Component from './Component'
+
+describe('test', () => {
+  it('works', () => {
+    let wrapper = mount(Component)
+
+    expect(true).toBe(true)
+  })
+})
