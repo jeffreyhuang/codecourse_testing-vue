14e118edb3aa87fe823a1195ff7cc167d9ba8bd8
Jeffrey Huang <jeffreyhuang0331@gmail.com>
2019-06-28 16:36:28 +0800

    [ch5] test template classes

---
+++ src/components/elements/tag/__test__/AppTag.spec.js
@@ -0,0 +1,35 @@
+import { mount } from '@vue/test-utils'
+import AppTag from '../AppTag'
+
+describe('AppTag', () => {
+  it('renders out the tag text', () => {
+    let wrapper = mount(AppTag, {
+      propsData: {
+        text: 'Vue'
+      }
+    })
+
+    expect(wrapper.html()).toContain('Vue')
+  })
+
+  it('renders the plain theme by default', () => {
+    let wrapper = mount(AppTag, {
+      propsData: {
+        text: 'Vue'
+      }
+    })
+
+    expect(wrapper.classes()).toEqual(['tag'])
+  })
+
+  it('renders the plain theme by default', () => {
+    let wrapper = mount(AppTag, {
+      propsData: {
+        text: 'Vue',
+        dark: true
+      }
+    })
+
+    expect(wrapper.classes()).toContain('tag--dark')
+  })
+})
