57fcc17044460ff5013a0a66b8f03b435d62526b
Jeffrey Huang <jeffreyhuang0331@gmail.com>
2019-07-15 16:16:07 +0800

    [ch6] add tests on click event

--- src/components/elements/reveal/__test__/AppReveal.spec.js
+++ src/components/elements/reveal/__test__/AppReveal.spec.js
@@ -3,5 +3,26 @@ import AppReveal from '../AppReveal'
 
 describe('AppReveal', () => {
   it('renders the text on click', () => {
+    let wrapper = mount(AppReveal, {
+      propsData: {
+        text: 'Hello'
+      }
+    })
+
+    wrapper.find('a').trigger('click')
+
+    expect(wrapper.html()).toContain('Hello')
+  })
+
+  it('hides the original anchor when clicked', () => {
+    let wrapper = mount(AppReveal, {
+      propsData: {
+        text: 'Hello'
+      }
+    })
+
+    wrapper.find('a').trigger('click')
+
+    expect(wrapper.find('a').exists()).toBe(false)
   })
 })
